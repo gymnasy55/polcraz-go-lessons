fe8ea337f99cc5b60856f32a53c4d8d5b4b3a0de
Kubariev Ilya <polcrazpolcraz@gmail.com>
2021-05-01 21:37:27 +0300

    003

--- 002_package/nested/nested.go
+++ 002_package/nested/nested.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/gymnasy55/polcraz-go-lessons/002_package/nested/hello"
-	"github.com/gymnasy55/polcraz-go-lessons/002_package/nested/say"
 )
 
 func main() {
 	fmt.Println("Start program...")
-	fmt.Println(hello.CallFromHello() + say.CallFromSay())
+	//fmt.Println(hello.CallFromHello() + say.CallFromSay())
 }
